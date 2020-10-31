e054ff339f80694659f94b713725a02af48d0628
keshav.lawande-SC <keshav.lawande-SC@airliquide.com>
2020-10-31 19:58:13 +0530

    Adding component to show API result

---
+++ components/ResultComponent.tsx
@@ -0,0 +1,7 @@
+import React, { FunctionComponent } from 'react';
+
+const ResultComponent: FunctionComponent = () => {
+    return <div></div>;
+};
+
+export default ResultComponent;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,15 @@ import Head from 'next/head';
 
 import styles from '../styles/Home.module.css';
 import FormComponent from '../components/FormComponent';
+import ResultComponent from '../components/ResultComponent';
 
 const Home: FunctionComponent = () => {
-    return <FormComponent />;
+    return (
+        <div>
+            <FormComponent />
+            <ResultComponent />
+        </div>
+    );
 };
 
 export default Home;
