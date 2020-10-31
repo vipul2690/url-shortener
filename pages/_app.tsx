0f2469ca0c5308cd33656fddc342e6a5953852e1
keshav.lawande-SC <keshav.lawande-SC@airliquide.com>
2020-10-31 19:32:15 +0530

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }): JSX.Element {
+    return <Component {...pageProps} />;
+}
+
+export default MyApp;
